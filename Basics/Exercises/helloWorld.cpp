d0e75a840e01e2b076f4f27339b56b960ae7e93a
Nerf-Chamber <lzpobletpobletos@gmail.com>
2026-01-16 08:56:07 +0100

    First commit CPP

---
+++ Basics/Exercises/helloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "helloWorld"
+
+using namespace std;
+
+void helloWorld() {
+    cout << "Hello world!";
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "helloWorld.h"
+
+using namespace std;
+
+int main() {
+    helloWorld();
+    return 0;
+}
